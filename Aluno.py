23529c8924e0e2edf6acd2c3793b6880b865c762
ANDREY BARRA <barraand2@gmail.com>
2026-05-07 19:50:29 -0300

    Criação da classe Aluno

---
+++ Aluno.py
@@ -0,0 +1,9 @@
+class Aluno():
+    def __init__(self, nome, email, senha, telefone):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.telefone = telefone
+
+    def login(self, email, senha):
+        print("Login realizado com sucesso!")    
